c52526928045d41df334699e0f38b751907ec284
wkaleb23 <wkaleb23@uw.edu>
2023-10-23 20:40:38 -0700

    All deck functions

--- a3.py
+++ a3.py
@@ -1,21 +1,20 @@
+import random
+
 ## These constants have been defined as a starting point.
 ## You may find yourself wanting to define things a little differently for your
 ##  implementation. That's fine.
 NUM_CARDS_IN_HAND = 5
 NUM_CARDS_IN_DECK = 24
 
-SUITS = ["Clubs", "Spades", "Hearts", "Diamonds"]
-FACES = ["Jack", "Queen", "King", "Ace"]
-NUMBERED = [9, 10]
-COLOR_BLACK = "Black"
-COLOR_RED = "Red"
+SUIT_NAMES = ["Clubs", "Spades", "Hearts", "Diamonds"]
+SUIT_COLORS = ["Black", "Black", "Red", "Red"]
+FACES = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+NUMBERED = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
 
 ## card is a tuple of (name, suit, color, value)
-
 ## deck is a list structured as a stack
-
 ## card_node is a dict of {next_card, prev_card, payload}
-
 ## hand is a linked list, represented by a dict of {first_card, num_cards_in_hand}
 
 # ----------------------------------------
@@ -24,86 +23,136 @@ COLOR_RED = "Red"
 #  Assume that the value of cards are:
 #  Nine=9; Ten=10; Jack=11; and so on, up to Ace=14.
 
+# shuffles the deck, returns a pointer to the deck
+def shuffle_deck(deck):
+    for i in range(0, 7):
+        random.shuffle(deck)
+    return deck
+
+
+# determines if the specified card is in the deck, returns 0 if the card is not in the deck, 1 otherwise
+def is_card_in_deck(deck, card):
+    for i in range(0, len(deck)):
+        if deck[i] == card:
+            return 1
+    return 0
+
+
 # Creates the deck, initializing any fields necessary.
 # Returns a deck.
 def create_deck():
-	pass
+    deck = []
+    for s in range(0, 4):
+        suit = SUIT_NAMES[s]
+        suit_color = SUIT_COLORS[s]
+        for i in range(0, 13):
+            card = (FACES[i], suit, suit_color, NUMBERED[i])
+            deck.append(card)
+    deck = shuffle_deck(deck)
+    return deck
+
 
 # Adds a card to the top of the deck.
 # Returns a pointer to the deck.
 def push_card_to_deck(deck, card):
-	pass
+    if is_card_in_deck(deck, card) == 1:
+        print("Card is already in deck")
+        return deck
+    # check that the deck is not full
+    if len(deck) == NUM_CARDS_IN_DECK:
+        print("Deck is full")
+        return deck
+    # add the card to the top of the deck
+    deck.append(card)
+    # return a pointer to the deck
+    return deck
+
 
 # Shows the top card, but does not remove it from the stack.
 # Returns a pointer to the top card.
 def peek_card(deck):
-	pass
+    return deck[-1]
+
 
 # Removes the top card from the deck and returns it.
 # Returns a pointer to the top card in the deck.
 def pop_card(deck):
-	pass
+    return deck.pop(0)
+
 
 # Determines if the deck is empty.
 # Returns 0 if the Deck has any cards; 1 otherwise.
 def is_deck_empty(deck):
-	pass
+    if len(deck) == 0:
+        return 1
+    else:
+        return 0
+
 
 ## Prints the provided deck
 def print_deck(deck):
-	pass
+    print(deck)
 
-#----------------------------------------
+
+# ----------------------------------------
 # Hand functions
-#----------------------------------------
+# ----------------------------------------
 
 ## A Hand is a linked list, so we define Card_Nodes before
 ## defining the Hand
 
 def create_card_node(card):
-	pass
+    pass
+
 
 def get_next_card_node(card_node):
-	pass
+    pass
+
 
 def get_prev_card_node(card_node):
-	pass
+    pass
+
 
 def get_card_from_node(card_node):
-	pass
+    pass
 
 
 # Creates a Hand and initializes any necessary fields.
 # Returns a new empty hand
 def create_hand():
-	pass
+    pass
+
 
 # Adds a card to the hand.
 def add_card_to_hand(hand, card):
-	pass
+    pass
+
 
 # Removes a card from the hand via card value
 # Returns the card (not a card_node) that was removed from the hand
 # Returns None if the specified card is not in the hand
 def remove_card_from_hand(hand, card):
-	pass
+    pass
+
 
 # Removes a card from the hand via index
 # Returns the card, not a card_node
 # Returns None if index is < 0 or greater than the length of the hand/list.
 def get_card_from_hand(hand, index):
-	pass
+    pass
+
 
 # Determines if there are any cards in the hand.
 # Return 0 if the hand is empty; 1 otherwise.
 def is_hand_empty(hand):
-	pass
+    pass
+
 
 # Prints a single card
 def print_card(card):
-	pass
+    pass
+
 
 # Prints an entire hand
 def print_hand(hand):
-	pass
-
+    pass
