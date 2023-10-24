5317145934845389b039a80a6d4eec30b442a658
wkaleb23 <wkaleb23@uw.edu>
2023-10-23 21:33:40 -0700

    All hand functions

--- a3.py
+++ a3.py
@@ -54,6 +54,7 @@ def create_deck():
 
 # Adds a card to the top of the deck.
 # Returns a pointer to the deck.
+# added checks to see if deck is full or if card is already in deck
 def push_card_to_deck(deck, card):
     if is_card_in_deck(deck, card) == 1:
         print("Card is already in deck")
@@ -102,57 +103,108 @@ def print_deck(deck):
 ## defining the Hand
 
 def create_card_node(card):
-    pass
+    card_node = {'next': None, 'prev': None, 'payload': card}
+    return card_node
 
 
 def get_next_card_node(card_node):
-    pass
+    return card_node['next']
 
 
 def get_prev_card_node(card_node):
-    pass
+    return card_node['prev']
 
 
 def get_card_from_node(card_node):
-    pass
+    return card_node['payload']
 
 
 # Creates a Hand and initializes any necessary fields.
 # Returns a new empty hand
 def create_hand():
-    pass
+    hand = {'first_card': None, 'num_cards_in_hand': 0}
+    return hand
 
 
 # Adds a card to the hand.
+# Added checks to see if hand is full
 def add_card_to_hand(hand, card):
-    pass
+    # Only add a card if the hand is not full
+    if hand['num_cards_in_hand'] == NUM_CARDS_IN_HAND:
+        print("Hand is full")
+    else:
+        hand['num_cards_in_hand'] += 1
+        card_node = create_card_node(card)
+        if hand['first_card'] is None:
+            # if the hand is empty, add the card to the hand
+            hand['first_card'] = card_node
+        else:
+            # if the hand is not empty, add the card to the beginning of the hand
+            card_node['next'] = hand['first_card']
+            hand['first_card']['prev'] = card_node
+            hand['first_card'] = card_node
 
 
 # Removes a card from the hand via card value
 # Returns the card (not a card_node) that was removed from the hand
 # Returns None if the specified card is not in the hand
 def remove_card_from_hand(hand, card):
-    pass
+    card_node = hand['first_card']
+    while card_node is not None:
+        if card_node['payload'] == card:
+            hand['num_cards_in_hand'] -= 1
+            if card_node['prev'] is not None:
+                card_node['prev']['next'] = card_node['next']
+            if card_node['next'] is not None:
+                card_node['next']['prev'] = card_node['prev']
+            if card_node == hand['first_card']:
+                hand['first_card'] = card_node['next']
+            return get_card_from_node(card_node)
+        card_node = card_node['next']
+    return None
+
+
+def is_card_in_hand(hand, card):
+    # determines if the specified card is in the hand, returns 0 if the card is not in the hand, 1 otherwise
+    # hand is a linked list, represented by a dict of {first_card, num_cards_in_hand}
+    # card is a tuple of (name, suit, color, value)
+    card_node = hand['first_card']
+    while card_node is not None:
+        if card_node['payload'] == card:
+            return 1
+        card_node = card_node['next']
+    return 0
 
 
 # Removes a card from the hand via index
 # Returns the card, not a card_node
 # Returns None if index is < 0 or greater than the length of the hand/list.
 def get_card_from_hand(hand, index):
-    pass
+    card_node = hand['first_card']
+    if index < 0 or index > hand['num_cards_in_hand']:
+        return None
+    for i in range(0, index):
+        card_node = card_node['next']
+    return get_card_from_node(card_node)
 
 
 # Determines if there are any cards in the hand.
 # Return 0 if the hand is empty; 1 otherwise.
 def is_hand_empty(hand):
-    pass
+    if hand['num_cards_in_hand'] == 0:
+        return 0
+    else:
+        return 1
 
 
 # Prints a single card
 def print_card(card):
-    pass
+    print(card)
 
 
 # Prints an entire hand
 def print_hand(hand):
-    pass
+    card_node = hand['first_card']
+    while card_node is not None:
+        print_card(get_card_from_node(card_node))
+        card_node = card_node['next']
